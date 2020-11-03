dd6d9d519fab593e7e44a2d85671442d44894bce
Foster Horak <fosterhorak@macbook-pro.attlocal.net>
2020-11-03 16:58:49 -0600

    pseudocoded js

---
+++ main.js
@@ -0,0 +1,42 @@
+  /*----- constants -----*/
+
+// fill the grid with...
+
+// winning combos...
+
+
+
+/*----- app's state (variables) -----*/
+// gameBoard, turn, winner
+
+/*----- cached element references -----*/
+// board, button, message
+
+/*----- event listeners -----*/
+// clicking the game board
+// clicking the reset button
+
+/*----- functions -----*/
+
+//initialize function
+  //gameBoard, whose turn, winner
+  //render
+
+//render function
+  //hide reset button
+  //fill gameBoard
+  //display message
+
+//handleClick function
+  //don't register if box already been clicked
+  //don't register if there is already a winner
+  //assign new image to box on gameBoard
+  //siwtch the turn
+  //render
+
+
+// check for winner function
+    //check absolute value of winning combos
+    //assign winner to who's turn it is...
+    //check for tie??
+

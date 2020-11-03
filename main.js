80280a5d94b753923ec57678ad4109592628bf96
Foster Horak <fosterhorak@macbook-pro.attlocal.net>
2020-11-03 15:32:08 -0600

    pseudo code js

--- main.js
+++ main.js
@@ -1,34 +1,79 @@
 
 // 1) Define required constants
-
 // 2) Define required variables used to track the state of the game
-
-// 3) Store elements on the page that will be accessed in code more than once in variables to make code more concise, readable and performant.
-
+// 3) Store elements on the page that will be accessed in code more 
+    // than once in variables to make code more concise, readable 
+    // and performant.
 // 4) Upon loading the app should:
 // 	4.1) Initialize the state variables
 // 	4.2) Render those values to the page
 // 	4.3) Wait for the user to click a square
-
 // 5) Handle a player clicking a square
-
 // 6) Handle a player clicking the replay button
 
 
 
 
 /*----- constants -----*/
-
+// winning combinations, players & their values
 
 /*----- app's state (variables) -----*/
+// who's turn it is, the game board, winner, scores
 
 /*----- cached element references -----*/
+// the boxes, and the text message
 
 /*----- event listeners -----*/
-
+//clicking the game board (moves)
+//clicking the play again button
+//clicking the reset button
 
 
 /*----- functions -----*/
+initialize();
 
 
+function initialize() {
+//set state variables
+    //game goard
+    //determine who's turn it is
+    //winner
 
+//render
+
+}
+
+function render() {
+    //fill the grid appropriately
+    //update message
+        //if there's a winner
+            //update scores
+        //if it's a ite
+        //if it's someones' turn
+}
+
+//function for clicking the board (moves)
+function handleMove() {
+    //get box index
+    //if available continue, if not - return
+    //update board
+    //check for winner
+    //update turn
+    //render
+}
+
+
+//fuction for clicking reset
+function reset(){
+
+}
+
+//function for clicking play again
+function playAgain() {
+
+}
+
+//function for checking for a winner
+function checkWinner() {
+    
+}

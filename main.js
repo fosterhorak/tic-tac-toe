41c2b4e3a01f54f1437264660a1f9e5c0ca7de60
Foster Horak <fosterhorak@macbook-pro.attlocal.net>
2020-11-03 15:49:21 -0600

    added constants, state variables, cache elements, event listners

--- main.js
+++ main.js
@@ -17,16 +17,40 @@
 /*----- constants -----*/
 // winning combinations, players & their values
 
+const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+]
+
+const players = {
+    '1': "url('https://media2.giphy.com/media/3o6ZsVbs2GzgKNvVpS/giphy.gif?cid=ecf05e47qp8333q5ikp2yjlyb1dts6d8qn9soocmj9cq2szy&rid=giphy.gif')",
+    '-1': "url('https://media2.giphy.com/media/rWgLOxrdNNDzUXaQnd/giphy.gif?cid=ecf05e47vjskno3mgni8p3paagefb10yz3ysrjw8200931p6&rid=giphy.gif')",
+    'null': 'white';
+}
+
 /*----- app's state (variables) -----*/
 // who's turn it is, the game board, winner, scores
+let turn, gameBoard, winner, scores;
 
 /*----- cached element references -----*/
 // the boxes, and the text message
+let boxes = document.querySelectorAll('td div');
+let message = document.getElementById('result-message')
+
 
 /*----- event listeners -----*/
 //clicking the game board (moves)
+document.querySelector('table').addEventListener('click', handleMove);
 //clicking the play again button
+document.getElementById('play-again').addEventListener('click', playAgain);
 //clicking the reset button
+document.getElementById('reset').addEventListener('click', reset);
 
 
 /*----- functions -----*/
@@ -75,5 +99,5 @@ function playAgain() {
 
 //function for checking for a winner
 function checkWinner() {
-    
+
 }
